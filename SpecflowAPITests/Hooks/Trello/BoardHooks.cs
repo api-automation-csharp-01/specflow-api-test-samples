883819dda03b063473495a82eea55d6652a5ac38
Alejandra Neolopan <aneolopan@ecisolutions.com>
2020-10-01 21:40:34 -0500

    [develop] Alejandra Neolopan - Trello tests

---
+++ SpecflowAPITests/Hooks/Trello/BoardHooks.cs
@@ -0,0 +1,29 @@
+﻿using SpecflowAPITests.Client;
+using SpecflowAPITests.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SpecflowAPITests.Hooks.Trello
+{
+    [Binding]
+    public sealed class BoardHooks
+    {
+        private Helper _helper;
+        public BoardHooks(Helper helper)
+        {
+            _helper = helper;
+        }
+
+        
+        [AfterScenario(Order = 101)]
+        [Scope(Tag = "deleteTrelloBoard")]
+        public void AfterScenario()
+        {
+            foreach (var id in _helper.GetIds())
+            {
+                var request = new TrelloRequest("boards/" + id);
+
+                RequestManager.Delete(TrelloClient.GetInstance(), request);
+            }
+        }
+    }
+}
